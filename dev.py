f2a3ed8467883d1a06c306392d50ff73321adcae
paynesa <paynesa@sas.upenn.edu>
2020-11-29 23:16:12 -0500

    calculating from frequency

--- dev.py
+++ dev.py
@@ -8,34 +8,45 @@ def remove_boundaries(input : str, delim : str)->str:
             output += char
     return output
 
-def get_transitional_probabilities(input: str, sub_boundary: str):
+def get_transitional_probabilities(input: str, sub_boundary: str)->Dict[str, float]:
+    """Gets the transitional probabilities from the input and returns a dictionary of syllables
+    separated by spaces, mapped to the transitional probabilities of the two syllables"""
     total_frequency : Dict[str, int] = {}
     transitional_frequency : Dict[str, int] = {}
     curr_syl: str = ""
     last_syl: str = ""
     for char in input:
-        # this means we have reached a boundary
+        # If we've reached a boundary, update our counts
         if char == sub_boundary:
             # update the total counts
             if curr_syl not in total_frequency:
                 total_frequency[curr_syl] = 0
+            total_frequency[curr_syl] += 1
             # only update the transitional frequency if we're not at a boundary
             if curr_syl and last_syl:
-                transitional : str = last_syl+curr_syl
+                transitional : str = last_syl+" "+curr_syl
                 if transitional not in transitional_frequency:
                     transitional_frequency[transitional] = 0
                 transitional_frequency[transitional] += 1
-            total_frequency[curr_syl] += 1
+            # update the current and last syllables
             last_syl = curr_syl
             curr_syl = ""
-        # TODO: want to kind of reset here
+        # If there's an utterance boundary, don't count transitional probabilities across it
         elif char == "U":
             curr_syl = ""
             last_syl = ""
+        # Otherwise, append to the current syllable
         else:
             curr_syl += char
-    print(total_frequency)
-    print(transitional_frequency)
+    # now that we have the transitional frequency and total frequency, we calculate TP(A->B) = P(AB)/P(A)
+    transitional_probabilities : Dict[str, float] = {}
+    for transition in transitional_frequency:
+        # get the transitional frequency of B following A
+        p_AB = transitional_frequency[transition]
+        # get the total frequency of A and set the transitional probability accordingly
+        p_A = total_frequency[transition.split()[0]]
+        transitional_probabilities[transition] = p_AB/p_A
+    return transitional_probabilities
 
 
 dev_input = "bPih1PgPSWdPrPah1PmPSWbPih1PgPSWdPrPah1PmPSWU"
