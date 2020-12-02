18b16b5832460b241f0aa7da878593365b5e8c81
paynesa <paynesa@sas.upenn.edu>
2020-12-01 23:18:35 -0500

    cleanup

--- run_slm.py
+++ run_slm.py
@@ -1,4 +1,5 @@
 from typing import Dict, List
+import argparse
 
 
 def remove_boundaries(input: str, delim: str) -> str:
@@ -31,36 +32,31 @@ def predict_word_boundaries(
             output += "W"
         output += syllables[i + 1] + "S"
     output += "W"
-    print(syllables)
-    print(tps)
-    print(output)
     return output
 
 
 def get_transitional_probabilities(input: str, sub_boundary: str) -> Dict[str, float]:
     """Gets the transitional probabilities from the input and returns a dictionary of syllables
-    separated by spaces, mapped to the transitional probabilities of the two syllables"""
+    separated by underscores, mapped to the transitional probabilities of the two syllables"""
     total_frequency: Dict[str, int] = {}
     transitional_frequency: Dict[str, int] = {}
-    # iterate through each utterance separately to get the overall frequency and transitional frequencies
-    for utterance in [input]:  # [utt for utt in input.split("U") if utt]:
-        # iterate through each syllable in the utterance
-        syllables: List[str] = [syll for syll in utterance.split(sub_boundary) if syll]
-        i: int = 0
-        while i < len(syllables):
-            curr_syl = syllables[i]
-            # update the frequency of the given syllable
-            if curr_syl not in total_frequency:
-                total_frequency[curr_syl] = 0
-            total_frequency[curr_syl] += 1
-            # if not word initial, update the transitional probabilities
-            if i > 0:
-                transitional: str = syllables[i - 1] + "_" + curr_syl
-                if transitional not in transitional_frequency:
-                    transitional_frequency[transitional] = 0
-                transitional_frequency[transitional] += 1
-            i += 1
-    # now, calculate the transitional probabilities based off of the frequencies
+    # iterate through each syllable separately to get the overall frequency and transitional frequencies
+    syllables: List[str] = [syll for syll in input.split(sub_boundary) if syll]
+    i: int = 0
+    while i < len(syllables):
+        curr_syl = syllables[i]
+        # update the frequency of the given syllable
+        if curr_syl not in total_frequency:
+            total_frequency[curr_syl] = 0
+        total_frequency[curr_syl] += 1
+        # if not word initial, update the transitional probabilities
+        if i > 0:
+            transitional: str = syllables[i - 1] + "_" + curr_syl
+            if transitional not in transitional_frequency:
+                transitional_frequency[transitional] = 0
+            transitional_frequency[transitional] += 1
+        i += 1
+    # now, calculate and return the transitional probabilities based off of the frequencies
     return {
         transition: (
             transitional_frequency[transition]
@@ -94,51 +90,15 @@ def main():
     total_correct = 0
     total_generated = 0
     total_right = 0
-
     for i in range(0, len(test_correct)):
         correct = [w for w in test_correct[i].split("W") if w]
         generated = [w for w in test_generated[i].split("W") if w]
-        total_right += len([w for w in generated if w in correct])
+        total_right += len([w for w in correct if w in generated])
         total_correct += len(correct)
         total_generated += len(generated)
     print(total_right / total_generated)
     print(total_right / total_correct)
     return
-
-    # get the correct utterances and the utterances to generate from
-    correct_utterances = [utt for utt in input.split("U") if utt]
-    utterances_no_boundaries = [
-        utt for utt in input_without_word_boundaries.split("U") if utt
-    ]
-    i: int = 0
-    recall = 0
-    precision = 0
-    total_precision_numerator = 0
-    total_precision_denominator = 0
-    total_recall_denominator = 0
-    # iterate through the utterances and generate for each using the transitional probabilities
-    while i < len(correct_utterances):
-        hypothesized: str = predict_word_boundaries(
-            utterances_no_boundaries[i], "S", "W", transitional_probabilities
-        )
-        # get the correct and hypothesized words and tally up the precision and recall
-        correct_words = [word for word in correct_utterances[i].split("W") if word]
-        hypothesized_words = [word for word in hypothesized.split("W") if word]
-        num_correct = len(
-            [word for word in correct_words if word in hypothesized_words]
-        )
-        print(len(correct_words), num_correct, len(hypothesized_words))
-        recall += num_correct / len(correct_words)
-        # TODO: what's going on here?
-        precision += num_correct / len(hypothesized_words)
-        total_precision_denominator += len(hypothesized_words)
-        total_precision_numerator += num_correct
-        total_recall_denominator += len(correct_words)
-        i += 1
-    print(recall / i)
-    print(precision / i)
-    print(total_precision_numerator / total_precision_denominator)
-    print(total_precision_numerator / total_recall_denominator)
 
 
 if __name__ == "__main__":
